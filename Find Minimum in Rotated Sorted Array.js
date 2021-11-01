e4a6f545efa3aa3748c84a9b7c7e93df8230294a
BADR KACIMI <31140256+BADRKAC@users.noreply.github.com>
2021-11-01 17:05:25 +0100

    Update Find Minimum in Rotated Sorted Array.js

--- Find Minimum in Rotated Sorted Array.js
+++ Find Minimum in Rotated Sorted Array.js
@@ -1,1 +1,29 @@
-
+#JavaScript
+var findMin = function(nums) {
+    
+        // Special case
+    if (nums === null || nums.length === 0) {
+        return -1;
+    }
+    
+    let left = 0;
+    let right = nums.length - 1;
+    // cibler pivot de rotation bach t3raf fin t9lab
+    
+    while (left < right) {
+        // Middle pointer
+        let middle = left + parseInt((right - left) / 2);
+        console.log(middle);
+        console.log(nums[middle]);
+        if (nums[middle] > nums[right]) {
+            left = middle + 1; // akhir valeur b9at hna hiya pivot
+        }
+        else {
+            right = middle; // rja3 lor rak khliti pivot wra lmiddle
+        }
+    }
+    return nums[left];
+    
+};
+______________________________________________________________________________
+#Java
